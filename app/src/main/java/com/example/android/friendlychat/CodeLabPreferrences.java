fefe5c8e4a5823e25342f03a5bfbd284494152a1
DITSIBISO <ditsibisomasete@gmail.com>
2017-07-12 13:43:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/friendlychat/CodeLabPreferrences.java
@@ -0,0 +1,11 @@
+package com.example.android.friendlychat;
+
+/**
+ * Created by eKasiLab Alex CDTB on 2017/07/12.
+ */
+
+public class CodeLabPreferrences {
+
+    public static final String INSTANCE_ID_TOKEN_RETRIEVED = "iid_token_retrieved";
+    public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
+}
